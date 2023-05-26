4fdddec888d5c7b327616d7560c56f9764439796
Mariane Barbosa Caldeira <87836763+MarianeCaldeira@users.noreply.github.com>
2023-05-26 11:14:41 -0300

    Update exercicio2.py

--- aula4/exercicio2.py
+++ aula4/exercicio2.py
@@ -5,34 +5,40 @@ class Conta:
         self.conta = conta
         self.saldo = 0
         self.status = False
+        self.limite = 0
         self.tipo = tipo
+        self.limiteUsado = 0
 
     def depositar(self, valord):
         if self.status == True:
             print(f"O dinheiro foi depositado!")
             self.saldo = valord
+            if self.limiteUsado > 0:
+                print("O dinheiro foi depositado e descontado na sua divida de limite!")
+                self.saldo = self.saldo - self.limiteUsado
+                self.limiteUsado = self.limiteUsado - self.saldo
+
         else:
             print(f'O dinheiro não foi depositado pois a conta está inativa!')
-            self.status = False
 
     def sacar(self, valorS):
         if self.status == False:
             print(f"Não foi possível sacar o dinheiro pois a conta está inativa!")
-            self.status = False
         else:
-            if self.saldo == 0:
-                print(f"Não foi possível sacar o dinheiro pois a conta não tem saldo!")
-            else:
-                print(f'O dinheiro foi sacado com sucesso')
+            if valorS < self.saldo:
+                print(f'O dinheiro foi sacado com sucesso!')
                 self.saldo = self.saldo - valorS
+            else:
+                calculo = valorS - self.saldo
+                self.limiteUsado = self.limiteUsado + calculo
+                self.saldo = 0
+                print(f"O dinheiro foi sacado com sucesso!")
 
     def verificarSaldo(self, ):
         if self.status == True:
-            print(f'O saldo da conta é {self.saldo}!')
-            self.status = True
+            print(f'O saldo da conta é {self.saldo} e o saldo usado é {self.limiteUsado}!')
         else:
             print(f'Não é possível ver o saldo pois a conta está inativa!')
-            self.status = False
 
     def ativar(self, ):
         if self.status == False:
@@ -40,7 +46,6 @@ class Conta:
             self.status = True
         else:
             print(f'Não foi possível ativar a conta pois a mesma ja está ativada!')
-            self.status = True
 
     def desativar(self, ):
         if self.status == False:
@@ -53,18 +58,28 @@ class Conta:
                 print(f'A conta foi desativada!')
                 self.status = False
 
+    def ativarLimite(self, valorL):
+        if self.status == True:
+            print(f"Limite ativado, seu limite é {valorL}!")
+            self.limite = valorL
+        else:
+            print("Limite não pode ser ativado pois a conta não está ativa!")
+
+    def desativarLimite(self):
+        if self.limite > 0:
+            print(f'Seu limite foi desativado!')
+            self.limite = 0
+        else:
+            print(f'Você não tem limite ativo!')
+
 p2 = Conta(955280, "Mariane", "DACC")
 p2.ativar()
-p2.ativar()
-p2.desativar()
-p2.ativar()
-p2.depositar(10)
-p2.desativar()
-p2.depositar(15)
-p2.ativar()
-p2.depositar(45)
-p2.sacar(45)
+p2.ativarLimite(10)
 p2.verificarSaldo()
-p2.sacar(45)
-p2.desativar()
-p2.desativar()
+p2.depositar(100)
+p2.verificarSaldo()
+p2.sacar(110)
+p2.verificarSaldo()
+p2.depositar(20)
+p2.verificarSaldo()
+p2.sacar(10)
